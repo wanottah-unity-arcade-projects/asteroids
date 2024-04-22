d0c5978945ecd5b195f7ab8d16ae65c352a1179d
Wanottah <wanottah@gmail.com>
2024-04-22 13:40:30 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public int score { get; private set; }
+    public int lives { get; private set; }
+
+    [SerializeField] private Player player;
+    [SerializeField] private ParticleSystem explosionEffect;
+    [SerializeField] private GameObject gameOverUI;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text livesText;
+
+    private void Awake()
+    {
+        if (Instance != null) {
+            DestroyImmediate(gameObject);
+        } else {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (lives <= 0 && Input.GetKeyDown(KeyCode.Return)) {
+            NewGame();
+        }
+    }
+
+    private void NewGame()
+    {
+        Asteroid[] asteroids = FindObjectsOfType<Asteroid>();
+
+        for (int i = 0; i < asteroids.Length; i++) {
+            Destroy(asteroids[i].gameObject);
+        }
+
+        gameOverUI.SetActive(false);
+
+        SetScore(0);
+        SetLives(3);
+        Respawn();
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        scoreText.text = score.ToString();
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+        livesText.text = lives.ToString();
+    }
+
+    private void Respawn()
+    {
+        player.transform.position = Vector3.zero;
+        player.gameObject.SetActive(true);
+    }
+
+    public void OnAsteroidDestroyed(Asteroid asteroid)
+    {
+        explosionEffect.transform.position = asteroid.transform.position;
+        explosionEffect.Play();
+
+        if (asteroid.size < 0.7f) {
+            SetScore(score + 100); // small asteroid
+        } else if (asteroid.size < 1.4f) {
+            SetScore(score + 50); // medium asteroid
+        } else {
+            SetScore(score + 25); // large asteroid
+        }
+    }
+
+    public void OnPlayerDeath(Player player)
+    {
+        player.gameObject.SetActive(false);
+
+        explosionEffect.transform.position = player.transform.position;
+        explosionEffect.Play();
+
+        SetLives(lives - 1);
+
+        if (lives <= 0) {
+            gameOverUI.SetActive(true);
+        } else {
+            Invoke(nameof(Respawn), player.respawnDelay);
+        }
+    }
+
+}
